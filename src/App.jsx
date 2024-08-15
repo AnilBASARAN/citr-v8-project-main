78651d988b299936a3521172ef95aef0d1aad1cc
Anıl Başaran <nlbsrn@gmail.com>
2024-08-15 07:50:45 +0300

    adopt pet project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SeachParams";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
