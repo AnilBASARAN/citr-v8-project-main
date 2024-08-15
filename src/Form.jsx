087ead316b89f5e67aea9585c45a85dd50ac1db5
Anıl Başaran <nlbsrn@gmail.com>
2024-08-15 08:11:25 +0300

    ı created this to have a seperate file in which contains the form.jsx seperately

---
+++ src/Form.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const Form = (props) => {
+const {animal,breed,breeds,requestPets,setAnimal,setBreed,setLocation,location} = props;
+    return (
+        <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <button>Submit</button>
+      </form>
+    )
+}
+
+export default Form;
